766c7ca4890bb78007b89aeafd0371186e8e7a21
RaguinAlexian <93644782+RaguinAlexian@users.noreply.github.com>
2022-03-22 10:20:24 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
 
-int main()
+// Exo 1
+
+/*int main()
 {
     int choix;
-    std::cout << "Veuillez choisir votre chemin, 1 étant celui de droite et 2 celui de gauche." << std::endl;
-    std::cin >> choix;
+    while(choix != 1 || choix !=2){
+        std::cout << "Veuillez choisir votre chemin, 1 étant celui de droite et 2 celui de gauche." << std::endl;
+        std::cin >> choix;
+    }
     if (choix == 1)
     {
         std::cout << "Vous avez choisi le chemin de droite" << std::endl;
@@ -15,4 +19,25 @@ int main()
         std::cout << "Vous avez choisi le chemin de gauche" << std::endl;
     }
     return 0;
-}
+} */
+
+// Exo 2
+
+/* int main()
+{
+    std::string choix;
+    while (choix != "gauche" || choix != "droite")
+    {
+        std::cout << "Veuillez choisir votre chemin, 'droite' ou 'gauche'." << std::endl;
+        std::cin >> choix;
+    }
+    if (choix == "droite")
+    {
+        std::cout << "Vous avez choisi le chemin de droite" << std::endl;
+    }
+    else if (choix == "gauche")
+    {
+        std::cout << "Vous avez choisi le chemin de gauche" << std::endl;
+    }
+    return 0;
+} */
